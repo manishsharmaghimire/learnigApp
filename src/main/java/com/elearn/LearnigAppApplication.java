2aeb9af282e72c3a93a6c113624dd7c58d0ae9b9
Manish Sharma Ghimire <manishsharmaghimire0@gmail.com>
2025-05-18 18:34:06 +0545

    Initial commit

---
+++ src/main/java/com/elearn/LearnigAppApplication.java
@@ -0,0 +1,13 @@
+package com.elearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnigAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnigAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elearn/LearnigAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnigAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
