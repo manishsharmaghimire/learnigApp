37bd2705e5dcd2d5a446cf5a6da7d95fd0def00e
Manish Sharma Ghimire <manishsharmaghimire0@gmail.com>
2025-05-22 17:27:54 +0545

    Ad-104: added missing  parameters and annotation in category

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,7 @@
 AD-101: added User entity with basic fields
 Ad-102: added docker-compose yaml with oracle config
 AD-103: added category entity
+Ad-104: added missing  parameters and annotation in category
 
 ###Changed
 
--- src/main/java/com/elearn/entity/Category.java
+++ src/main/java/com/elearn/entity/Category.java
@@ -1,11 +1,21 @@
 package com.elearn.entity;
 
 import jakarta.persistence.Column;
-
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import java.util.Date;
 
+@Entity
+@Table(name="categories")
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
 public class Category {
-
     private String id;
     private String title;
     @Column(name = "desc")
